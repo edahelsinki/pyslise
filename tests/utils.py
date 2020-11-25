7f0294feed72ea7821fd8eeaf37202eae9fd4a1a
Aggrathon <antonbjo@gmail.com>
2020-11-25 11:16:56 +0200

    pytest

---
+++ tests/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import numpy.random as npr
+
+
+def data_create(n: int, d: int) -> (np.ndarray, np.ndarray):
+    X = npr.normal(npr.normal(size=d)[np.newaxis,], 1.0, [n, d])
+    Y = npr.normal(size=n) + npr.normal()
+    return X, Y
+
+
+def data_create2(n: int, d: int) -> (np.ndarray, np.ndarray):
+    X = npr.normal(npr.normal(size=d)[np.newaxis,], 1.0, [n, d])
+    mod = npr.normal(size=d)
+    Y = X @ mod + npr.normal(size=n, scale=0.1)
+    return X, Y, mod
